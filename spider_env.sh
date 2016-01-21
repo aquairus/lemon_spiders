92c22560588fffba4fb151459c9c19e8b5214bd6
aquairus <18650013469@163.com>
2016-01-21 17:28:05 +0800

    abort

--- spider_env.sh
+++ spider_env.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/bash
-chmod 700 ~/.ssh 
-cp id_rsa.pub ~/.ssh/id_rsa.pub
-cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 
 apt-get install python-dev libxml2 libxml2-dev python-lxml -y #python-devel
 pip install requests beautifulsoup4 threadpool pybloom  bitarray lxml
