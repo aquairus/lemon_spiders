cb8038201eb1564b7c8cd6664c59124aba3b9993
aquairus <18650013469@163.com>
2016-02-17 18:44:16 +0800

    save

---
+++ douban/douban/pipelines.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+from scrapy.exceptions import DropItem
+import json
+import sys
+
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
+
+
+class DoubanPipeline(object):
+    def open_spider(self, spider):
+        self.of=open("../../douban.txt",'w+')
+
+    def process_item(self, item, spider):
+        dic=dict(item)
+        dic.pop("tid")
+        dic.pop("total")
+        self.of.write(json.dumps(dic, ensure_ascii=False)+"\n")
+
+    def close_spider(self, spider):
+        self.of.close()
+
+
+class mergePipeline(object):
+
+    def __init__(self):
+        self.content = dict()
+        self.total = dict()
+        self.answers=dict()
+
+
+    def process_item(self, item, spider):
+        tid=item["tid"]
+
+        if "total" in item:
+            if item["total"]=="1":
+#                print "short topic"
+                return item
+            else:
+                self.content[tid]=item.copy()
+                self.total[tid]=item["total"]
+                raise DropItem("content")
+
+
+        if not tid in self.answers:
+            self.answers[tid]={}
+
+        pid=int(item["pid"])/100
+        self.answers[tid][pid]=item["answers"]
+
+        keys=self.answers[tid].keys()
+    #    print str(len(keys))+"/"+self.total[tid]
+
+
+        if len(keys)+1==int(self.total[tid]):
+            for (v,c) in self.answers[tid].items():
+                self.content[tid]["answers"]=\
+                self.content[tid]["answers"]+c
+            douban=self.content[tid].copy()
+            self.total.pop(tid)
+            self.answers.pop(tid)
+            self.content.pop(tid)
+    #        print "long!!"
+            return douban
+        else:
+            raise DropItem("answers")
---
+++ douban/douban/spiders/douban.py
@@ -0,0 +1,108 @@
+from scrapy.spiders import Spider
+from scrapy.selector import Selector
+from ..items import topicItem,ansItem
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from bs4 import BeautifulSoup
+import re
+
+# regex = re.compile(r"novelId=(\d*)&volumeNo=(\d*)")
+topic_re = re.compile(r"/topic/(\d*)/")
+answers_re=re.compile(r"/topic/(\d*)/\?start=(\d*)")
+
+class doubanSpider(CrawlSpider):
+    name = "douban"
+    allowed_domains = ["douban.com"]
+    start_urls = ["https://www.douban.com/group/explore"
+     ]
+
+    rules=(Rule(LinkExtractor(allow=('(.*?)explore/\w*$')) ),\
+            Rule(LinkExtractor(allow=('(.*?)group/\w*/$')),\
+                         callback='parse_url',follow=True ),
+             Rule(LinkExtractor(allow=('(.*?)\w\\?start=\d*$')),\
+              callback='parse_url',follow=True),\
+            Rule(LinkExtractor(allow=('(.*?)topic/\d*/$')),\
+                         callback='parse_topic',follow=True ),
+            Rule(LinkExtractor(allow=('(.*?)topic/\d*/\\?start=\d*00$')),\
+                         callback='parse_answers',follow=True ),
+
+     )
+
+
+    def parse_url(self, response):
+        pass
+
+
+    def parse_topic(self, response):
+        m=topic_re.search(response.url)
+        tid=m.group(1)
+
+        title=response.css('h1::text').extract()[0]
+        author=response.xpath('//h3/span/a/text()').extract()[0]
+        time=response.css('span[class=color-green]::text').extract()[0]
+
+        paras=response.xpath("//div[@class='topic-content']/p/text()").extract()
+        topic_content=""
+        for p in paras:
+            topic_content=topic_content+p+"<p>"
+
+        try:
+            text=response.css('ul[class=topic-reply]').extract()[0]
+            answers=get_answers(text)
+        except BaseException, e:
+            answers=[]
+
+        try:
+            total=response.css('span[class=thispage]::attr(data-total-page)').extract()[0]
+        except BaseException, e:
+            total="1"
+
+        topic=topicItem()
+        topic["title"]=title
+        topic["author"]=author
+        topic["time"]=time
+        topic["answers"]=answers
+        topic["topic_content"]=topic_content
+        topic["tid"]=tid
+        topic["total"]=total
+        return topic
+
+    def parse_answers(self, response):
+
+        m=answers_re.search(response.url)
+        tid=m.group(1)
+        pid=m.group(2)
+        text=response.css('ul[class=topic-reply]').extract()[0]
+        answers=get_answers(text)
+        item= ansItem()
+        item["tid"]=tid
+        item["pid"]=pid
+        item["answers"]=answers
+        return item
+
+
+
+def get_answers(text):
+    answers=[]
+    soup=BeautifulSoup(text,"lxml")
+    replys=soup.find_all('li')
+    for reply in replys:
+        ans=get_ans(reply)
+        answers.append(ans)
+    return answers
+
+def get_ans(li):
+    ans={}
+    answer=li.find("h4").a.text
+    time=li.find("h4").span.text
+    content=li.find("p").text
+    try:
+        quote_text=li.find("span",class_="short").text
+    except BaseException, e:
+        quote_text=""
+
+    ans["content"]=content
+    ans["quote_text"]=quote_text
+    ans["time"]=time
+    ans["answer"]=answer
+    return ans
