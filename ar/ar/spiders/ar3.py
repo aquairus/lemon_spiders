efb8d421c160cfd7fab72b88276a5e6e60f05ef8
aquairus <18650013469@163.com>
2016-03-11 18:11:07 +0800

    save

--- ar/ar/spiders/ar3.py
+++ ar/ar/spiders/ar3.py
@@ -13,27 +13,28 @@ time_re = re.compile(r"(\d+/)")
 
 class ar3Spider(CrawlSpider):
     name = "ar3"
-    allowed_domains = ["arabic-media.com"]
+    allowed_domains = ["alqabas.com"]
     start_urls = ["http://alqabas.com",
     "http://alqabas.com/category/حول-العالم/",
     "http://alqabas.com/category/ثقافة-وفنون/",
-    "http://alqabas.com/category/كتاب-وآراء/"
-    "http://alqabas.com/category/المرأة/"
+    "http://alqabas.com/category/كتاب-وآراء/",
+    "http://alqabas.com/category/المرأة/",
     "http://alqabas.com/category/القبس-الدولي/",
-    "http://alqabas.com/category/رياضة/"
+    "http://alqabas.com/category/رياضة/",
     "http://alqabas.com/category/اقتصاد/",
     "http://alqabas.com/category/أمن-ومحاكم/",
-    "http://alqabas.com/category/مجلس-الأمة/"
-    "http://alqabas.com/category/محليات/"
+    "http://alqabas.com/category/مجلس-الأمة/",
+    "http://alqabas.com/category/محليات/",
+    "http://alqabas.com/خريطة-الموقع/",
 
      ]
-
-    rules=(Rule(LinkExtractor(allow=('/\d{4}'),deny=('video|archive|slideshow|rss')) ,\
-                callback='parse_news',follow=True),\
-             Rule(LinkExtractor(allow=('category|tag'),deny=('video|slideshow|rss|login')),
+#,follow=True
+    rules=(Rule(LinkExtractor(allow=('/\d{3}'),deny=('201\d')) ,\
+                follow=True,callback='parse_news'),\
+             Rule(LinkExtractor(allow=('category|tag|date|page|author'),deny=('video|login|feed')),
              follow=True),
-             Rule(LinkExtractor(allow=('.*'),deny=('video|slideshow|rss|login')),
-             callback='parse_url'follow=True),
+             Rule(LinkExtractor(allow=('.*'),deny=('cvideo|login|feed|email')),
+             callback='parse_url',follow=True),
 
      )
 
@@ -43,27 +44,25 @@ class ar3Spider(CrawlSpider):
     def parse_news(self, response):
 
         url=response.url
-        print url
-        # try:
-        #     title=response.xpath("//h1[@class='heading-story']/text()").extract()[0].strip()
-        #     contents=response.xpath("//div[@id='DynamicContentContainer']/p").extract()
-        # except BaseException,e:
-        #     print e
-        #     print url
-        #     return
-        # #print title
-        #
-        # m=time_re.findall(url)
-        # time=m[0]+m[1]+m[2]
-        #
-        # content=""
-        # for c in contents:
-        #     content=content+c#"<p>"+c+"</p>"
-        #
-        # ar=arItem()
-        # ar["title"]=title
-        # ar["content"]=content
-        # ar["review"]=""
-        # ar["time"]=time
-        # ar["url"]=url
-        # return ar
+
+        try:
+            title=response.xpath("//span[@itemprop='name']/text()").extract()[0].strip()
+            contents=response.xpath("//div[@class='entry']/p").extract()
+        except BaseException,e:
+            print e
+            print url
+            return
+        time=response.xpath("//span[@class='tie-date']/text()").extract()[0]
+
+        #print title
+        content=""
+        for c in contents:
+            content=content+c
+
+        ar=arItem()
+        ar["title"]=title
+        ar["content"]=content
+        ar["review"]=""
+        ar["time"]=time
+        ar["url"]=url
+        return ar
