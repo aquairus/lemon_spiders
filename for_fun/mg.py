50111187ab08147d02f344c3c8a67df12b7192c5
aquairus <18650013469@163.com>
2016-03-30 13:51:00 +0800

    save

--- for_fun/mg.py
+++ for_fun/mg.py
@@ -2,7 +2,7 @@ import pymongo
 from pymongo import MongoClient
 import json
 import sys
-
+from time import sleep
 reload(sys)
 sys.setdefaultencoding( "utf-8" )
 print "name?"
@@ -11,25 +11,31 @@ name=raw_input()
 
 #name="ar3"
 client = MongoClient()
-client = MongoClient('spider05', 27017)
+client = MongoClient('spider08', 27017)
 db = client.ar
 
 yahoo=db.get_collection(name)
 
 
 
-
+duplicate=set() 
 cnt=0
 
 f =open ("../../"+name+".txt","w+")
 
 cursor=yahoo.find()
 for line in cursor:
-	#f.write(json.loads(line)+"\n")
 	line.pop("_id")
-	#print line	
-	#string=json.dumps(line, ensure_ascii=False)
-	f.write(json.dumps(line, ensure_ascii=False)+"\n")
+	url=line["url"]
+
+	if not url in duplicate:
+		f.write(json.dumps(line, ensure_ascii=False)+"\n")
+	else:
+		print "different"	
+
+	duplicate.add(url)
+	
+	print "set:"+str(len(duplicate))
 	cnt+=1
 	print cnt
 	
